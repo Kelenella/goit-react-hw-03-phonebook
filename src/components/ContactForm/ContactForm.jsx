06c45eb40e25a85e1c629fdfe21fce48820005dd
Jane Yushchenko <kelenella@ukr.net>
2021-10-22 19:10:23 +0300

    start files copied

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import s from './ContactForm.module.css';
+import PropTypes from 'prop-types';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  handleInputChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = e => {
+    const { name, number } = this.state;
+    e.preventDefault();
+    const contact = {
+      id: uuidv4(),
+      name,
+      number,
+    };
+    this.props.onSubmit(contact);
+    this.reset();
+  };
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+    const { handleSubmit, handleInputChange } = this;
+    return (
+      <form className={s.form} onSubmit={handleSubmit}>
+        <label className={s.label}>
+          Name
+          <input
+            className={s.input}
+            type="text"
+            value={name}
+            onChange={handleInputChange}
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+            required
+          />
+        </label>
+        <label>
+          Number
+          <input
+            className={s.input}
+            type="tel"
+            value={number}
+            onChange={handleInputChange}
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+          />
+        </label>
+        <button className={s.button} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
